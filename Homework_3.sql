029a137c5e47beb7461cb4bd15ff1d8bcfb481a6
jakubkoszewnik <159775968+jakubkoszewnik@users.noreply.github.com>
2024-02-12 12:24:50 +0100

    Update Homework_3.sql

--- Homework_3.sql
+++ Homework_3.sql
@@ -1,3 +1,22 @@
+/* the task was :
+In the SQL query with CTE, combine data from these tables to obtain:
+
+ad_date: the date of displaying ads on Google and Facebook;
+campaign_name: the name of the campaign on Google and Facebook;
+spend, impressions, reach, clicks, leads, value: campaign metrics and ad set metrics on specific days.
+Similar to the task in the previous topic, create a sample from the resulting combined table (CTE):
+
+ad_date: date of ad display
+campaign_name: campaign name
+Aggregate values for the following metrics grouped by date and campaign_name:
+Total cost,
+Number of impressions,
+Number of clicks,
+Total conversion value.
+To accomplish this task, group the table by the ad_date and campaign_name fields.
+*/
+
+
 WITH a AS (
 SELECT fabd.ad_date, 
 fc.campaign_name,

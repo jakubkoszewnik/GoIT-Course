97e8f7aaea8a7a900c90802b2c7d160c2fc93813
jakubkoszewnik <159775968+jakubkoszewnik@users.noreply.github.com>
2024-02-12 12:19:30 +0100

    Update Homework_1.sql

--- Homework_1.sql
+++ Homework_1.sql
@@ -1,4 +1,5 @@
-SELECT ad_date, spend, clicks, spend / clicks AS spend_click_Ratio
+SELECT ad_date, spend, clicks, spend / clicks AS spend_click_Ratio /* extracting data and making spend/click metric */
 FROM public.facebook_ads_basic_daily
-WHERE  clicks > 0
+WHERE  clicks > 0    /* boundaries */
 ORDER BY ad_date DESC
+/* simple code to get familiarized with SQL,  */
